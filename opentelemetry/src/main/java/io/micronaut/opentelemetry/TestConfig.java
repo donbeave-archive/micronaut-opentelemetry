6972a95f5d1cdbaaf716c58d443afd4ed233a72e
Alexey Zhokhov <alexey@zhokhov.com>
2021-05-31 05:08:41 +0800

    Test config.

--- opentelemetry/src/main/java/io/micronaut/opentelemetry/TestConfig.java
+++ opentelemetry/src/main/java/io/micronaut/opentelemetry/TestConfig.java
@@ -1,5 +1,6 @@
 package io.micronaut.opentelemetry;
 
+import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
 import io.opentelemetry.api.OpenTelemetry;
@@ -8,12 +9,16 @@ import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.SpanProcessor;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_NAME;
 
 // TODO REMOVE ME PLEASE
 
@@ -27,6 +32,7 @@ public class TestConfig {
     @Singleton
     public SpanExporter otelSpanExporter() {
         return OtlpGrpcSpanExporter.builder().build();
+        //return OtlpGrpcSpanExporter2.builder().build();
     }
 
     @Bean
@@ -36,11 +42,16 @@ public class TestConfig {
         return SimpleSpanProcessor.create(spanExporter);
     }
 
+    @Inject
     @Bean
     @Singleton
-    public SdkTracerProvider otelSdkTraceProvider(SpanProcessor spanProcessor) {
+    public SdkTracerProvider otelSdkTraceProvider(SpanProcessor spanProcessor, ApplicationContext applicationContext) {
+        String applicationName =
+                applicationContext.getProperty("micronaut.application.name", String.class).orElse(io.micronaut.context.env.Environment.DEFAULT_NAME);
+
         return SdkTracerProvider.builder()
                 .addSpanProcessor(spanProcessor)
+                .setResource(Resource.getDefault().toBuilder().put(SERVICE_NAME, applicationName).build())
                 .build();
     }
 
